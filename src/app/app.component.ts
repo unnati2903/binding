bea279bf3c6865cc2db872d43d1eb5f08111e2cb
Unnati Panchal <unnatip@fermion.in>
2022-07-08 17:21:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bindingAssignment';
+  textInterpolation:string="this is example of text interpolation";
+  propertyBinding:string="this is example of property binding";
+  buttonAction:string="attribute binding";
+  hero = "classBinding";
+  g = document.getElementsByClassName(this.hero);
+  twoWayBinding:string="two way binding";
+  
+    EventBiding(){
+      alert("this is example of event binding");
+    }
 }
